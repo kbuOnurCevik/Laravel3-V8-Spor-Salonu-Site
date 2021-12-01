7744c5d08e9c479d2c11054744fd8bce70886f04
kbuOnurCevik <2017010213001@ogrenci.karabuk.edu.tr>
2021-12-01 14:28:53 +0300

    Admin Login İslemleri

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,6 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 
 
 Route::get('/', function () {
@@ -22,7 +10,11 @@ Route::get('/', function () {
 });
 
 //Admin
-Route::get('/admin',[\App\Http\Controllers\Admin\HomeController::class,'index'])->name('adminhome');
+Route::get('/admin',[\App\Http\Controllers\Admin\HomeController::class,'index'])->name('admin_home')->middleware('auth');
+
+Route::get('/admin/login',[\App\Http\Controllers\Admin\HomeController::class,'login'])->name('admin_login');
+Route::post('/admin/logincheck',[\App\Http\Controllers\Admin\HomeController::class,'logincheck'])->name('admin_logincheck');
+Route::get('/admin/logout',[\App\Http\Controllers\Admin\HomeController::class,'logout'])->name('admin_logout');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
