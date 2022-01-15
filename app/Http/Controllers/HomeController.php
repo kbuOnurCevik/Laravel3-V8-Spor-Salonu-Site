e995b50e68a4d8353a7bdcc061182e0f155cb324
kbuOnurCevik <2017010213001@ogrenci.karabuk.edu.tr>
2022-01-16 01:57:40 +0300

    Status control

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,11 +18,15 @@ class HomeController extends Controller
 {
     //
 
-    public static function categoryList(){
-        return Category::where('parent_id', '=', 0)->with('children')->get();
+    public static function categoryList()
+    {
+        return Category::where('status', '=', 'true')->where('parent_id', '=', 0)->with('children', function ($q) {
+            $q->where('status', '=', 'true');
+        })->get();
     }
 
-    public static function getsetting(){
+    public static function getsetting()
+    {
         return Setting::first();
     }
 
@@ -30,10 +34,10 @@ class HomeController extends Controller
     {
 
         $setting = Setting::first();
-        $slider = Product::select('id','title', 'image', 'price','month','slug')->limit(4)->get();
-        $daily = Product::select('id','title', 'image', 'price','month','slug')->limit(3)->inRandomOrder()->get();
-        $last = Product::select('id','title', 'image', 'price','month','slug')->limit(3)->orderByDesc('id')->get();
-        $picked = Product::select('id','title', 'image', 'price','month','slug')->limit(3)->inRandomOrder()->get();
+        $slider = Product::where('status', '=', 'true')->select('id', 'title', 'image', 'price', 'month', 'slug')->limit(4)->get();
+        $daily = Product::where('status', '=', 'true')->select('id', 'title', 'image', 'price', 'month', 'slug')->limit(3)->inRandomOrder()->get();
+        $last = Product::where('status', '=', 'true')->select('id', 'title', 'image', 'price', 'month', 'slug')->limit(3)->orderByDesc('id')->get();
+        $picked = Product::where('status', '=', 'true')->select('id', 'title', 'image', 'price', 'month', 'slug')->limit(3)->inRandomOrder()->get();
         #print_r($picked);
         # exit();
         $data = [
@@ -44,60 +48,61 @@ class HomeController extends Controller
             'last' => $last,
             'page' => 'home'
         ];
-        return view('home.index',$data);
+        return view('home.index', $data);
     }
 
-    public function product($id,$slug)
+    public function product($id, $slug)
     {
         $data = Product::find($id);
-        $datalist = Image::where('product_id',$id)->get();
-        $reviews = \App\Models\Review::where('product_id',$id)->get();
+        $datalist = Image::where('product_id', $id)->get();
+        $reviews = \App\Models\Review::where('product_id', $id)->where('status', '=', 'true')->get();
         #print_r($data);
         #exit();
-        return view('home.product_detail',['data'=>$data,'datalist'=>$datalist,'reviews'=>$reviews]);
+        return view('home.product_detail', ['data' => $data, 'datalist' => $datalist, 'reviews' => $reviews]);
     }
 
     public function getproduct(Request $request)
     {
         $search = $request->input('search');
-        $count = Product::where('title','like','%'.$search.'%')->get()->count();
+        $count = Product::where('status', '=', 'true')->where('title', 'like', '%' . $search . '%')->get()->count();
 
-        if($count==1){
-            $data = Product::where('title','like','%'.$search.'%')->first();
-            return redirect()->route('product',['id'=>$data->id,'slug'=>$data->slug]);
-        }else{
-            return redirect()->route('productlist',['search'=>$search]);
+        if ($count == 1) {
+            $data = Product::where('status', '=', 'true')->where('title', 'like', '%' . $search . '%')->first();
+            return redirect()->route('product', ['id' => $data->id, 'slug' => $data->slug]);
+        } else {
+            return redirect()->route('productlist', ['search' => $search]);
         }
 
     }
 
     public function productlist($search)
     {
-        $datalist = Product::where('title','like','%'.$search.'%')->get();
-        return view('home.search_products',['search'=>$search,'datalist'=>$datalist]);
+        $datalist = Product::where('title', 'like', '%' . $search . '%')->where('status', '=', 'true')->get();
+        return view('home.search_products', ['search' => $search, 'datalist' => $datalist]);
     }
 
-    public function categoryproducts($id,$slug)
+    public function categoryproducts($id, $slug)
     {
-        $datalist = Product::where('category_id',$id)->get();
+        $datalist = Product::where([['category_id', $id], ['status', '=', 'true']])->get();
         $data = Category::find($id);
         #print_r($data);
         #exit();
-        return view('home.category_products',['data'=>$data,'datalist'=>$datalist]);
+        return view('home.category_products', ['data' => $data, 'datalist' => $datalist]);
     }
 
 
     public function aboutus()
     {
         $setting = Setting::first();
-        return view('home.about',['setting'=>$setting]);
+        return view('home.about', ['setting' => $setting]);
     }
 
     public function faq()
     {
-        $datalist = Faq::all()->sortBy('position');
-        return view('home.faq',['datalist'=>$datalist]);
+        $datalist = Faq::where('status', '=', 'true')->get()->sortBy('position');
+        return view('home.faq', ['datalist' => $datalist]);
     }
+
     public function schedule()
     {
         return view('home.schedule');
@@ -106,7 +111,7 @@ class HomeController extends Controller
     public function contact()
     {
         $setting = Setting::first();
-        return view('home.contact',['setting'=>$setting]);
+        return view('home.contact', ['setting' => $setting]);
     }
 
     public function sendmessage(Request $request)
@@ -120,21 +125,22 @@ class HomeController extends Controller
 
         $data->save();
 
-        return redirect()->route('contact')->with('info','Mesajınız Kaydedilmiştir. Teşekkürler !');
+        return redirect()->route('contact')->with('info', 'Mesajınız Kaydedilmiştir. Teşekkürler !');
 
     }
 
 
-    public function login(){
+    public function login()
+    {
         return view('home.index');
     }
 
     public function logincheck(Request $request)
     {
 
-        if ($request->isMethod('post')){
-            $credentials = $request->only('email','password');
-            if(Auth::attempt($credentials)){
+        if ($request->isMethod('post')) {
+            $credentials = $request->only('email', 'password');
+            if (Auth::attempt($credentials)) {
                 $request->session()->regenerate();
 
                 return redirect()->intended('home');
@@ -143,13 +149,13 @@ class HomeController extends Controller
             return back()->withErrors([
                 'email' => 'The provided credentials do not match our records.',
             ]);
-        }
-        else{
+        } else {
             return view('admin.login');
         }
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         Auth::logout();
 
         $request->session()->invalidate();
